a6224e3ffb2f01e5dce6e5069d88dd6ec369b95b
TristanBlackwell <tristanblackwell1@gmail.com>
2022-02-03 08:19:23 +0000

    Include count of posts on blog index page

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -5,21 +5,26 @@ import { ISOToFriendlyDate } from "~/utils/helpers";
 
 type LoaderData = {
   blogPosts: { title: string; created_at: Date; slug: string }[];
+  count: number;
 };
 
 export const loader: LoaderFunction = async () => {
-  const data: LoaderData = {
-    blogPosts: await db.posts.findMany({
-      take: 5,
-      select: { title: true, created_at: true, slug: true },
-      orderBy: { created_at: "desc" },
-    }),
+  const posts = await db.posts.findMany({
+    take: 5,
+    select: { title: true, created_at: true, slug: true },
+    orderBy: { created_at: "desc" },
+  });
+
+  const count = await db.posts.count();
+
+  return {
+    blogPosts: posts,
+    count,
   };
-  return data;
 };
 
 export default function Index() {
-  const posts = useLoaderData<LoaderData>();
+  const postsData = useLoaderData<LoaderData>();
 
   return (
     <div className="px-44 lg:px-24 md:px-16 sm:px-8">
@@ -28,11 +33,12 @@ export default function Index() {
           Latest Posts
         </h1>
         <p className="text-lg text-soft-white tracking-wide xs:mt-3">
-          <span className="text-gold font-bold">22</span> Articles
+          <span className="text-gold font-bold">{postsData.count}</span>{" "}
+          Articles
         </p>
       </div>
       <section className="grid grid-cols-2 gap-24 mt-12 sm:grid-cols-1 md:gap-8">
-        {posts.blogPosts.map((post) => {
+        {postsData.blogPosts.map((post) => {
           return (
             <div
               key={post.created_at + post.title}
