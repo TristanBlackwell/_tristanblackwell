5ff8eb1697592e55481454fd5e6f65c00a668c02
TristanBlackwell <tristanblackwell1@gmail.com>
2022-02-06 15:21:35 +0000

    Improve error handling for blog index page

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,12 +1,12 @@
 import { ChevronRightIcon } from "@heroicons/react/outline";
-import { Link, LoaderFunction, useLoaderData } from "remix";
+import { Link, LoaderFunction, useCatch, useLoaderData } from "remix";
 import supabase from "~/services/supabase.service";
 import { ISOToFriendlyDate } from "~/utils/helpers";
 import { PostMeta } from "~/types";
 
 type LoaderData = {
-  blogPosts: PostMeta[];
-  count: number;
+  blogPosts: PostMeta[] | null;
+  count: number | null;
 };
 
 export const loader: LoaderFunction = async () => {
@@ -16,15 +16,15 @@ export const loader: LoaderFunction = async () => {
     .order("created_at", { ascending: true })
     .limit(5);
 
-  const { count, error: countError } = await supabase
+  const { count } = await supabase
     .from("posts")
     .select("id", { count: "exact", head: true });
 
-  if (postError || countError) {
+  if (postError) {
+    throw new Response("Unable to load posts. Please try again later.", {
+      status: 500,
+    });
   }
-
-  console.log(posts);
-  console.log(count);
 
   return {
     blogPosts: posts,
@@ -36,17 +36,19 @@ export default function Index() {
   const postsData = useLoaderData<LoaderData>();
 
   return (
-    <div className="px-44 lg:px-24 md:px-16 sm:px-8">
-      <div className="flex justify-between items-baseline xs:flex-col">
-        <h1 className="font:archivo text-2xl text-soft-white font-bold tracking-wider">
+    <div className="px-44 sm:px-8 md:px-16 lg:px-24">
+      <div className="xs:flex-col flex items-baseline justify-between">
+        <h1 className="font:archivo text-soft-white text-2xl font-bold tracking-wider">
           Latest Posts
         </h1>
-        <p className="text-lg text-soft-white tracking-wide xs:mt-3">
-          <span className="text-gold font-bold">{postsData.count}</span>{" "}
+        <p className="text-soft-white xs:mt-3 text-lg tracking-wide">
+          <span className="text-gold font-bold">
+            {postsData.count ? postsData.count : 0}
+          </span>{" "}
           Articles
         </p>
       </div>
-      <section className="grid grid-cols-2 gap-24 mt-12 sm:grid-cols-1 md:gap-8">
+      <section className="mt-12 grid grid-cols-2 gap-24 sm:grid-cols-1 md:gap-8">
         {postsData.count === 0 ? (
           <div>
             <p className="font-archivo text-soft-white text-xl">
@@ -54,23 +56,24 @@ export default function Index() {
             </p>
           </div>
         ) : (
+          postsData.blogPosts &&
           postsData.blogPosts.map((post) => {
             return (
               <div
                 key={post.created_at + post.title}
-                className="bg-slight-blue rounded-md pt-4 px-4 pb-1 shadow-2xl"
+                className="bg-slight-blue rounded-md px-4 pt-4 pb-1 shadow-2xl"
               >
                 <p className="font-light">
                   {ISOToFriendlyDate(post.created_at)}
                 </p>
                 <Link to={post.slug} prefetch="intent">
-                  <h2 className="text-xl text-soft-white tracking-wide font-archivo font-bold mb-2 cursor-pointer">
+                  <h2 className="text-soft-white font-archivo mb-2 cursor-pointer text-xl font-bold tracking-wide">
                     {post.title}
                   </h2>
                 </Link>
                 <p className="mb-4">{post.excerpt}</p>
                 <Link to={post.slug}>
-                  <div className="flex text-soft-white hover:text-gold transition-colors cursor-pointer">
+                  <div className="text-soft-white hover:text-gold flex cursor-pointer transition-colors">
                     <span className="italic">see more</span>
                     <ChevronRightIcon className="h-6 w-6 hover:translate-x-1 hover:transition-transform hover:ease-in-out" />
                   </div>
@@ -80,6 +83,39 @@ export default function Index() {
           })
         )}
       </section>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <div className="text-center">
+      <h2 className="font:archivo text-soft-white text-2xl font-bold tracking-wider">
+        {caught.data}
+      </h2>
+      <div className="mt-8">
+        <Link to="/" className="btn py-2">
+          Back to safety
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export function ErrorBoundary() {
+  return (
+    <div className="text-center">
+      <h2 className="font:archivo text-soft-white text-2xl font-bold tracking-wider">
+        Uh Oh,
+        <br /> Looks like something went wrong!
+      </h2>
+      <div className="mt-8">
+        <Link to="/" className="btn py-2">
+          Back to safety
+        </Link>
+      </div>
     </div>
   );
 }
